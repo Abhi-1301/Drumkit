04182a82ce0c26ea4fa0fe7b3ee3002c9b0d17bb
Abhi-1301 <90414121+Abhi-1301@users.noreply.github.com>
2021-09-09 23:41:27 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,66 @@
+var buttonnumber = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < buttonnumber; ++i) {
+	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+		var buttonhtml = this.innerHTML;
+		sound(buttonhtml); flash(buttonhtml);
+	});
+}
+
+document.addEventListener("keypress", function (Event) {
+	sound(Event.key); flash(Event.key);
+});
+
+function sound(key) {
+	switch (key) {
+		case "w":
+			var audio = new Audio("tom-1.mp3");
+			audio.play();
+			break;
+
+		case "a":
+			var audio = new Audio("tom-2.mp3");
+			audio.play();
+			break;
+
+		case "s":
+			var audio = new Audio("tom-3.mp3");
+			audio.play();
+			break;
+
+		case "d":
+			var audio = new Audio("tom-4.mp3");
+			audio.play();
+			break;
+
+		case "j":
+			var audio = new Audio("snare.mp3");
+			audio.play();
+			break;
+
+		case "k":
+			var audio = new Audio("crash.mp3");
+			audio.play();
+			break;
+
+		case "l":
+			var audio = new Audio("kick-bass.mp3");
+			audio.play();
+			break;
+
+		default:
+			console.log(key);
+	}
+}
+
+function flash(key) {
+	var activebutton = document.querySelector("." + key);
+	
+	activebutton.classList.add("pressed");
+	
+	setTimeout(function () {
+		activebutton.classList.remove("pressed")
+	}, 100);
+	
+}
